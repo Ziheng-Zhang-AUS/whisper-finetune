b211746e99a4a5c10aa7a9e5e5a56cdc7085f2fa
Zihang Zhang <u8038147@anu.edu.au>
2025-04-26 13:23:32 +1000

    change utils.py for load_dataset(), using json format instead of hf

--- src/whisper_finetune/data/utils.py
+++ src/whisper_finetune/data/utils.py
@@ -133,7 +133,7 @@ def process_dataset(dataset_names, select_n_per_ds, split_name, groupby_col):
     for N, GROUPBYCOL, dataset_name in zip(select_n_per_ds, groupby_col, dataset_names):
         # dataset = load_dataset(dataset_name, split=split_name)
         # use jsonl to load dataset instead of use huggingface
-        dataset = load_dataset("json", dataset_name, split=split_name)
+        dataset = load_dataset("json", data_files=dataset_name, split=split_name)
         original_size = len(dataset)
         print(f"Processing dataset: {dataset_name}")
         print(f"Original dataset size: {original_size}")
